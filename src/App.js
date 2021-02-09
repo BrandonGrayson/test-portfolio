3d80bac33a0834f13226211d49555c9b646fe7ee
Brandon <brandonbgrayson@gmail.com>
2021-02-09 18:53:10 -0500

    imported react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Hello from "../src/components/hello"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="">
+    <div>
       <Hello />
     </div>
   );
--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hello = () => {
     return (
-        <div className='container'>
+        <div className='container hero'>
             <h1>Hello.</h1>
             <h1>I Am</h1>
             <h1>Brandon</h1>
