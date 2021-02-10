d4807a82f0efc671395bf78ec1041f375efa7bd8
Brandon <brandonbgrayson@gmail.com>
2021-02-09 19:33:27 -0500

    added flex column to hello section and flex row for picture section

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const Hello = () => {
     return (
-        <div className='container hero'>
-            <h1>Hello.</h1>
-            <h1>I Am</h1>
-            <h1>Brandon</h1>
+        <div className='container hero d-flex flex-row'>
+            <div className='flex-column'>
+                <h1>Hello.</h1>
+                <h1>I Am</h1>
+                <h1>Brandon</h1>
+            </div>
 
-        
+
+
             <img src="./img/Brandon_cropped.jpg" class="img-thumbnail" alt="..."/>
         </div>
     )
