87674de972f1b22d25d16bed25e6b819c8bd152a
Brandon <brandonbgrayson@gmail.com>
2021-02-11 21:51:39 -0500

    card titles updated / added images for anime vs ufc and day planner

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,7 +18,24 @@ const Portfolio = () => {
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Dungeon Master Manager</h5>
+                                    <p className="card-text">DMM is a role playing game based off Dungeons and Dragons. The DMM can easily create Characters and design storylines. While keeping track of skills and progress.</p>
+                                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='projects'>
+                    <div className="card mb-3" style={{ "maxWidth": "540px" }}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src="..." alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">Anime Vs UFC</h5>
                                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
@@ -35,8 +52,8 @@ const Portfolio = () => {
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 className="card-title">Weather Planner</h5>
+                                    <p className="card-text">Weather Planner was designed to give users access to the 5 day forecast of any city.</p>
                                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
@@ -52,8 +69,8 @@ const Portfolio = () => {
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 className="card-title">Maze</h5>
+                                    <p className="card-text">Welcome to Maze Runner. True to its name this app is in fact a maze, Can you reach the goal?</p>
                                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
@@ -69,24 +86,7 @@ const Portfolio = () => {
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='projects'>
-                    <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                        <div className="row g-0">
-                            <div className="col-md-4">
-                                <img src="..." alt="..." />
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Day Planner</h5>
                                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
@@ -104,7 +104,7 @@ const Portfolio = () => {
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Generate Password</h5>
                                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
